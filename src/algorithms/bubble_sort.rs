3fde2614701647f85b420354f66f3b6d92d2073e
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-07 10:25:59 +0200

    using arr swap function

--- src/algorithms/bubble_sort.rs
+++ src/algorithms/bubble_sort.rs
@@ -1,14 +1,10 @@
-use std::mem;
-
 // function to sort an array of integers using bubble sort
 pub fn sort(arr: &mut [i32]) {
     let mut temp: i32;
     for i in 0..arr.len() {
         for j in 0..arr.len() - 1 {
             if arr[j] > arr[j + 1] {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                arr.swap(j, j + 1);
             }
         }
     }
