29b46c162f9bfff6b36740e0986413930a71df1a
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-07 10:14:47 +0200

    refactor bubble_sort::sort function to use for instead of while

--- src/algorithms/bubble_sort.rs
+++ src/algorithms/bubble_sort.rs
@@ -1,18 +1,15 @@
+use std::mem;
+
 // function to sort an array of integers using bubble sort
 pub fn sort(arr: &mut [i32]) {
-    let mut i = 0;
-    let mut j = 0;
-    let mut temp;
-    while i < arr.len() {
-        while j < arr.len() - 1 {
+    let mut temp: i32;
+    for i in 0..arr.len() {
+        for j in 0..arr.len() - 1 {
             if arr[j] > arr[j + 1] {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-            j += 1;
         }
-        i += 1;
-        j = 0;
     }
 }
