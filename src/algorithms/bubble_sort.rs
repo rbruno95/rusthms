14ffa6b0bcc4ca8c759d34e1f08a70af35125d66
Bruno <48922045+rbruno95@users.noreply.github.com>
2022-07-07 09:18:17 -0400

    clean bubble sort code

--- src/algorithms/bubble_sort.rs
+++ src/algorithms/bubble_sort.rs
@@ -1,8 +1,10 @@
 // function to sort an array of integers using bubble sort
+
+#[allow(dead_code)]
 pub fn sort(arr: &mut [i32]) {
-    let mut temp: i32;
-    for i in 0..arr.len() {
-        for j in 0..arr.len() - 1 {
+    let n = arr.len();
+    for _ in 0..n {
+        for j in 0..n - 1 {
             if arr[j] > arr[j + 1] {
                 arr.swap(j, j + 1);
             }
