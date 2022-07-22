f27d16c02faa0281e13d7ca56f8480500a7d30bf
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-22 22:11:25 +0200

    import directly sum::sum_first_n_natural_numbers

--- tests/math/sum.rs
+++ tests/math/sum.rs
@@ -1,20 +1,20 @@
-use rusthms::algorithms::math::sum;
+use rusthms::algorithms::math::sum::sum_first_n_natural_numbers;
 
 #[test]
 pub fn it_sum_first_n_natural_numbers() {
-    assert_eq!(0, sum::sum_first_n_natural_numbers(0));
-    assert_eq!(1, sum::sum_first_n_natural_numbers(1));
-    assert_eq!(3, sum::sum_first_n_natural_numbers(2));
-    assert_eq!(6, sum::sum_first_n_natural_numbers(3));
-    assert_eq!(10, sum::sum_first_n_natural_numbers(4));
-    assert_eq!(15, sum::sum_first_n_natural_numbers(5));
-    assert_eq!(21, sum::sum_first_n_natural_numbers(6));
-    assert_eq!(28, sum::sum_first_n_natural_numbers(7));
-    assert_eq!(36, sum::sum_first_n_natural_numbers(8));
-    assert_eq!(45, sum::sum_first_n_natural_numbers(9));
-    assert_eq!(55, sum::sum_first_n_natural_numbers(10));
-    assert_eq!(30628, sum::sum_first_n_natural_numbers(247));
-    assert_eq!(761995, sum::sum_first_n_natural_numbers(1234));
-    assert_eq!(59631757185, sum::sum_first_n_natural_numbers(345345));
-    assert_eq!(500000500000, sum::sum_first_n_natural_numbers(1000000));
+    assert_eq!(0, sum_first_n_natural_numbers(0));
+    assert_eq!(1, sum_first_n_natural_numbers(1));
+    assert_eq!(3, sum_first_n_natural_numbers(2));
+    assert_eq!(6, sum_first_n_natural_numbers(3));
+    assert_eq!(10, sum_first_n_natural_numbers(4));
+    assert_eq!(15, sum_first_n_natural_numbers(5));
+    assert_eq!(21, sum_first_n_natural_numbers(6));
+    assert_eq!(28, sum_first_n_natural_numbers(7));
+    assert_eq!(36, sum_first_n_natural_numbers(8));
+    assert_eq!(45, sum_first_n_natural_numbers(9));
+    assert_eq!(55, sum_first_n_natural_numbers(10));
+    assert_eq!(30628, sum_first_n_natural_numbers(247));
+    assert_eq!(761995, sum_first_n_natural_numbers(1234));
+    assert_eq!(59631757185, sum_first_n_natural_numbers(345345));
+    assert_eq!(500000500000, sum_first_n_natural_numbers(1000000));
 }
