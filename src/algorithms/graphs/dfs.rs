b8c9cdca79efc05fba28ebe49e6cc0551d2aa864
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-22 20:57:10 +0200

    refactor dfs method to use iteration instead indexation

--- src/algorithms/graphs/dfs.rs
+++ src/algorithms/graphs/dfs.rs
@@ -1,13 +1,10 @@
 #[allow(dead_code)]
-pub fn dfs(adj: &mut Vec<Vec<usize>>, vis: &mut Vec<bool>, v: usize, res: &mut Vec<usize>) {
-    if vis[v] {
-        return;
-    }
-    vis[v] = true;
-    res.push(v);
-    for u in 0..adj[v].len() {
-        if !vis[adj[v][u]] {
-            dfs(adj, vis, adj[v][u], res);
+pub fn dfs(adj: &Vec<Vec<usize>>, vis: &mut Vec<bool>, u: usize) {
+    vis[u] = true;
+    for v in adj[u].iter() {
+        let v = *v;
+        if !vis[v] {
+            dfs(adj, vis, v);
         }
     }
 }
