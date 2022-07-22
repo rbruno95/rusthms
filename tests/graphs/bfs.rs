0998fa40902384ae77486c5019896956eb902f33
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-22 22:09:00 +0200

    refactor vector initialization

--- tests/graphs/bfs.rs
+++ tests/graphs/bfs.rs
@@ -8,13 +8,14 @@ pub fn it_bfs_path_through_graph() {
     //    2     3
     //   /     / \
     //  0     4   1
-    let mut vec = vec![];
-    vec.push(vec![2]);
-    vec.push(vec![3]);
-    vec.push(vec![0, 5]);
-    vec.push(vec![1, 4, 5]);
-    vec.push(vec![3]);
-    vec.push(vec![2, 3]);
+    let mut vec = vec![
+        vec![2],
+        vec![3],
+        vec![0, 5],
+        vec![1, 4, 5],
+        vec![3],
+        vec![2, 3],
+    ];
 
     // Declaring visited array of len 6
     let mut vis = vec![false; 6];
