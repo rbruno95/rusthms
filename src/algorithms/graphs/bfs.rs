b53911ef7ef095732300db40cc6bce63c518bc41
Kamina <37193227+Segmentree@users.noreply.github.com>
2022-07-22 22:30:58 +0200

    feature/#13/bfs_algorithm (#14)

---
+++ src/algorithms/graphs/bfs.rs
@@ -0,0 +1,18 @@
+use std::collections::VecDeque;
+
+#[allow(dead_code)]
+pub fn bfs(adj: &Vec<Vec<usize>>, vis: &mut Vec<bool>, s: usize) {
+    let mut q = VecDeque::new();
+    q.push_back(s);
+    vis[s] = true;
+    while !q.is_empty() {
+        let u = q.pop_front().unwrap();
+        vis[u] = true;
+        for v in adj[u].iter() {
+            let v = *v;
+            if !vis[v] {
+                q.push_back(v);
+            }
+        }
+    }
+}
---
+++ tests/graphs/bfs.rs
@@ -0,0 +1,27 @@
+use rusthms::algorithms::graphs::bfs::bfs;
+
+#[test]
+pub fn it_bfs_path_through_graph() {
+    // Creating a graph with 6 vertices
+    //       5
+    //     /   \
+    //    2     3
+    //   /     / \
+    //  0     4   1
+    let mut vec = vec![
+        vec![2],
+        vec![3],
+        vec![0, 5],
+        vec![1, 4, 5],
+        vec![3],
+        vec![2, 3],
+    ];
+
+    // Declaring visited array of len 6
+    let mut vis = vec![false; 6];
+
+    bfs(&mut vec, &mut vis, 5);
+
+    // Asserting that vertices are visited
+    assert_eq!(vec![true; 6], vis);
+}
